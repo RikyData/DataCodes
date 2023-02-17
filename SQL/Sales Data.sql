74dba52210647b3eb26a9d48928cf74dce1858f4
RikyData <rikyindltd@yahoo.com>
2023-02-17 12:26:19 +0100

    Adding my SQL Work

---
+++ SQL/Sales Data.sql
@@ -0,0 +1,18 @@
+create table "sales_data"(
+"SALES_ID" numeric, 	
+"SALES_REP" text,	
+"EMAILS" varchar,	
+"BRANDS" text,	
+"PLANT_COST" numeric,
+"UNIT_PRICE" numeric,	
+"QUANTITY" numeric,	
+"COST" numeric,	
+"PROFIT" numeric,	
+"COUNTRIES" text,	
+"REGION" text, 	
+"MONTHS" text,	
+"YEARS" numeric
+);
+
+select *
+from "sales_data";
